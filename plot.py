8fc49b68d130c2f9065be316605869c78371a7d0
Marshall Bruner <brunerm99@gmail.com>
2021-04-27 12:11:34 -0600

    added function - converts polar coords to png

--- plot.py
+++ plot.py
@@ -2,19 +2,36 @@ import random
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+import pandas as pd
+import re
+import csv
 
 coord_queue = []
 coords = {}
 
-theta = np.linspace(-1 * math.pi / 2, math.pi / 2, num=180)
-r = []
-for index, angle in enumerate(theta):
-	r.append(random.random() * 10 + 5)
-	coords.update({angle: r[index]})
+# input
+# [0-9]+ 	[0-9]+\.?[0-9]*
+# theta		range
+def plot(fname):
+	theta = np.linspace(-1 * math.pi / 2, math.pi / 2, num=180)
+	range_arr = np.ma.masked_array(np.zeros(180), mask=True)
+	with open(fname) as coords:
+		reader = csv.reader(coords)
+		for row in reader:
+			temp_row = re.search('([0-9]+) ([0-9]+\.?[0-9]*)', str(row))
+			temp_angle = temp_row.group(1)
+			temp_range = temp_row.group(2)
+			range_arr[int(temp_angle)] = temp_range
 
-print(coords)
+	# Interpolation for angles without corresponding ranges
+	range_pd = pd.DataFrame(data=range_arr)
+	range_pd = range_pd.interpolate(method='linear', limit_direction='backward')
+	range_pd = range_pd.fillna(0)
+	range_arr = range_pd.to_numpy().reshape(range_arr.size)
 
-ax = plt.subplot(111, projection='polar')
-ax.plot(theta, r)
-ax.set_rmax(20)
-plt.savefig('test.png')
+	ax = plt.subplot(111, projection='polar')
+	ax.plot(theta, range_arr)
+	ax.set_rmax(30)
+	ax.set
+	plt.savefig(fname + '.png')
+	
